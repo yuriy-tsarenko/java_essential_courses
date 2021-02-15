e018ec2e8abb3fad36b39f60e6de01c1c4c96c67
Dmitry Kopaiev <htcakuma@gmail.com>
2021-02-15 21:38:59 +0200

    Feature: 4 Fix Maven

--- src/main/java/com/java_essential/kopaiev_hometasks/task1/Main.java
+++ src/main/java/com/java_essential/kopaiev_hometasks/task1/Main.java
@@ -1,5 +1,10 @@
 package com.java_essential.kopaiev_hometasks.task1;
 
+import com.java_essential.kopaiev_hometasks.task2.classWork.Student;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/com/java_essential/kopaiev_hometasks/task2/classWork/Student.java
+++ src/main/java/com/java_essential/kopaiev_hometasks/task2/classWork/Student.java
@@ -2,10 +2,71 @@ package com.java_essential.kopaiev_hometasks.task2.classWork;
 
 public class Student {
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     private String username;
     private String email;
-    private string phone;
-    private string name;
-    private string lastname;
+    private String phone;
+    private String name;
+    private String lastname;
     private int age;
+
+    public Student() {
+
+    }
 }
