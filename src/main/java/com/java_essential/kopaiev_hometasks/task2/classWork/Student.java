89acf51f4c8fdb54b4a893a717b3b676ccc8692e
Dmitry Kopaiev <htcakuma@gmail.com>
2021-02-15 20:34:32 +0200

    Feature: 2 Created packages, added tasks

--- src/main/java/com/java_essential/kopaiev_hometasks/task2/classWork/Student.java
+++ src/main/java/com/java_essential/kopaiev_hometasks/task2/classWork/Student.java
@@ -1,5 +1,11 @@
 package com.java_essential.kopaiev_hometasks.task2.classWork;
 
 public class Student {
-
+    private long id;
+    private String username;
+    private String email;
+    private string phone;
+    private string name;
+    private string lastname;
+    private int age;
 }
