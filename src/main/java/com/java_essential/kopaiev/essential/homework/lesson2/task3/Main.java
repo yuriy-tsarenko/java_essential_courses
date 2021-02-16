d53b139c9d1a6d514b1c9009a6db15b1d1148796
kudokuna <htcakuma@gmail.com>
2021-02-17 00:54:16 +0200

    Feature-8: Complete lesson2 home task

---
+++ src/main/java/com/java_essential/kopaiev/essential/homework/lesson2/task3/Main.java
@@ -0,0 +1,9 @@
+package com.java_essential.kopaiev.essential.homework.lesson2.task3;
+
+public class Main {
+    Car withoutParameterCar = new Car();
+    Car oneParameterCar = new Car(1995);
+    Car twoParameterCar = new Car(1994, 320);
+    Car threeParameterCar = new Car(1993,250, 2000);
+    Car fourParameterCar = new Car(2002, 220, 3500,"red");
+}
---
+++ src/main/java/com/java_essential/kopaiev/essential/homework/lesson2/task4/Car.java
@@ -0,0 +1,36 @@
+package com.java_essential.kopaiev.essential.homework.lesson2.task4;
+
+public class Car {
+    private int year;
+    private double speed;
+    private int weight;
+    private String color;
+
+    public Car()
+    {
+
+    }
+
+    public Car(int year)
+    {
+        this.year = year;
+    }
+
+    public Car (int year, double speed)
+    {
+        this(year);
+        this.speed = speed;
+    }
+
+    public Car (int year, double speed, int weight)
+    {
+        this(year,speed);
+        this.weight = weight;
+    }
+
+    public Car (int year, double speed, int weight, String color)
+    {
+        this(year, speed, weight);
+        this.color = color;
+    }
+}
