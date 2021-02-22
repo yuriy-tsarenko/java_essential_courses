805d2c63fc594e60ef1eb5b0d23b8ba3b68380e8
Nikita <4ered29nik@gmail.com>
2021-02-22 20:44:59 +0200

    FEATURE-8 classwork task_1

---
+++ src/main/java/com/java_essential/Cherednichenko/classwork/lesson_5/Array.java
@@ -0,0 +1,21 @@
+package com.java_essential.Cherednichenko.classwork.lesson_5;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Array {
+    public static void main(String[] args) {
+        Integer[] arrayInt = new Integer[]{1, 2, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> listInt;
+        listInt = Arrays.asList(arrayInt);
+        for (Integer integer : listInt) {
+            System.out.println(integer);
+        }
+        Object[] arrayIntnew = listInt.toArray();
+        for (Object integer : arrayIntnew) {
+            System.out.println(integer);
+        }
+    }
+}
