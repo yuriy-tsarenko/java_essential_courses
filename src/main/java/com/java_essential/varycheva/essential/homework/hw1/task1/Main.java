49fdb76267db91803a6b8754cec0a7b488c0eb77
Daryna <rinasaara@gmail.com>
2021-02-23 11:55:39 +0200

    Added fix for HW1

--- src/main/java/com/java_essential/varycheva/essential/homework/hw1/task1/Main.java
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw1/task1/Main.java
@@ -8,8 +8,12 @@ public class Main {
         double side1 = scan.nextDouble();
         double side2 = scan.nextDouble();
 
-        Rectangle rectangle = new Rectangle(side1, side2);
-        System.out.println(rectangle.areaCalculator());
-        System.out.println(rectangle.perimeterCalculator());
+        if (side1 <= 0 || side2 <= 0) {
+            System.out.println("Сторона должна быть положительным числом");
+        } else {
+            Rectangle rectangle = new Rectangle(side1, side2);
+            System.out.println(rectangle.areaCalculator());
+            System.out.println(rectangle.perimeterCalculator());
+        }
     }
 }
--- src/main/java/com/java_essential/varycheva/essential/homework/hw1/task1/Rectangle.java
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw1/task1/Rectangle.java
@@ -5,13 +5,9 @@ public class Rectangle {
     private double side1;
     private double side2;
 
-    public Rectangle(double side1, double side2){
-        if (side1 <= 0 || side2 <= 0){
-            throw new IllegalArgumentException("Сторона должна быть положительным числом");
-        } else {
-            this.side1 = side1;
-            this.side2 = side2;
-        }
+    public Rectangle(double side1, double side2) {
+        this.side1 = side1;
+        this.side2 = side2;
     }
 
     public double getSide1() {
