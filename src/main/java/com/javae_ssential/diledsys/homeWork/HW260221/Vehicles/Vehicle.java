957f7da1ab68c65642c404b00fb429e8ba229f35
diledsys <diledsys@gmil.com>
2021-02-26 12:19:09 +0300

    FEATURE-12: Commit HomeWork24.02.2021_Iterator

--- src/main/java/com/javae_ssential/diledsys/homeWork/HW260221/Vehicles/Vehicle.java
+++ src/main/java/com/javae_ssential/diledsys/homeWork/HW260221/Vehicles/Vehicle.java
@@ -1,24 +1,23 @@
 package com.javae_ssential.diledsys.homeWork.HW260221.Vehicles;
 
 public class Vehicle {
-    private Wheel wheel;
-   {
-        wheel = new Wheel();
-   }
-    public void print(){
+
+    public void print() {
 
     }
 
-    public static class Wheel extends Vehicle{
-        int count=0;
+    public class Wheel extends Vehicle {
+        int count = 0;
+
         @Override
         public void print() {
-            if (count==0) System.out.println("new copy");
+            if (count == 0) System.out.println("new copy");
             ++count;
-            System.out.println("Iam Wheel->"+count);
+            System.out.println("Iam Wheel->" + count);
         }
     }
-    public static class Door extends Vehicle{
+
+    public class Door extends Vehicle {
         @Override
         public void print() {
             System.out.println("Iam Door");
@@ -28,13 +27,9 @@ public class Vehicle {
 
     public static void main(String[] args) {
         // new copy
-        Vehicle wheel = new Wheel();
-        wheel.print(); //первый вызов
-        wheel.print(); //второй
-        //new copy
-        Vehicle wheel2 = new Wheel();
-        wheel2.print();
-        Vehicle door = new Door();
-        door.print();
+        Vehicle.Wheel wheel = new Vehicle().new Wheel();
+        Vehicle.Door door = new Vehicle().new Door();
+wheel.print();
+door.print();
     }
 }
