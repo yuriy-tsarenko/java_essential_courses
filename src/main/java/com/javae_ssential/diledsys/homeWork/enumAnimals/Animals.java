9480b6fedee3d446274d9a454d6ec362393949d3
diledsys <diledsys@gmil.com>
2021-02-27 14:09:15 +0300

    Commit HomeWork27.02.2021_enum

---
+++ src/main/java/com/javae_ssential/diledsys/homeWork/enumAnimals/Animals.java
@@ -0,0 +1,18 @@
+package com.javae_ssential.diledsys.homeWork.enumAnimals;
+
+public enum Animals {
+    TIGR(1),
+    CAT(5),
+    DOG(2);
+
+    public String toString() {
+        return "Animals" +"\n"+
+                "Tiger age = "+TIGR.age+"\n"+
+                "Cat age = " + CAT.age +"\n"+
+                "Dog age = "+ DOG.age;
+    }
+    private int age;
+    Animals(int age) {
+        this.age = age;
+    }
+}
