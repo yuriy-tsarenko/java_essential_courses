223cf1685c46db1860f9ea38167e98feac7b27d8
kudokuna <htcakuma@gmail.com>
2021-02-28 23:30:39 +0200

    Feature-1: initial commit

--- src/main/java/com/java_essential/kopaiev/essential/homework/lesson4/task3/Playable.java
+++
@@ -1,7 +0,0 @@
-package com.java_essential.kopaiev.essential.homework.lesson4.task3;
-
-public interface Playable {
-    void play();
-    void pause();
-    void stop();
-}
--- src/main/java/com/java_essential/kopaiev/essential/homework/lesson4/task3/Recordable.java
+++
@@ -1,7 +0,0 @@
-package com.java_essential.kopaiev.essential.homework.lesson4.task3;
-
-public interface Recordable {
-    void record();
-    void pause();
-    void stop();
-}
