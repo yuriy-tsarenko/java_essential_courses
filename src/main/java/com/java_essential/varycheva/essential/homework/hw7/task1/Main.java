188771ca17807f11085d9fbdcf515377a9472b49
Daryna <rinasaara@gmail.com>
2021-02-28 17:24:10 +0200

    Added hw7

---
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw7/task1/Main.java
@@ -0,0 +1,9 @@
+package com.java_essential.varycheva.essential.homework.hw7.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        for (Animals animal : Animals.values()) {
+            System.out.println(animal.toString());
+        }
+    }
+}
---
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw7/task2/Main.java
@@ -0,0 +1,9 @@
+package com.java_essential.varycheva.essential.homework.hw7.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        for (Vehicles vehicle : Vehicles.values()) {
+            System.out.println(vehicle.toString());
+        }
+    }
+}
