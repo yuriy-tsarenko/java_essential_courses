9b52a7dc187f155f6f6ba51c32fa86b5cbcb6579
Daryna <rinasaara@gmail.com>
2021-02-28 17:01:51 +0200

    Added hw6

---
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw6/task1/Vehicle.java
@@ -0,0 +1,21 @@
+package com.java_essential.varycheva.essential.homework.hw6.task1;
+
+public class Vehicle {
+
+
+    public void print() {
+        System.out.println("Vehicle");
+    }
+
+    public static class Wheel {
+        public void print() {
+            System.out.println("Wheel");
+        }
+    }
+
+    public static class Door {
+        public void print() {
+            System.out.println("Door");
+        }
+    }
+}
