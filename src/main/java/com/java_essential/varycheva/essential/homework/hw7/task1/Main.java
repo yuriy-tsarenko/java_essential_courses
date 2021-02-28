204d58db6b1e93f67b16601501a399366824a1f0
Daryna <rinasaara@gmail.com>
2021-02-28 17:28:20 +0200

    Added tasks descriptions

--- src/main/java/com/java_essential/varycheva/essential/homework/hw7/task1/Main.java
+++ src/main/java/com/java_essential/varycheva/essential/homework/hw7/task1/Main.java
@@ -1,5 +1,11 @@
 package com.java_essential.varycheva.essential.homework.hw7.task1;
 
+/*
+Создайте проект, используя IntelliJ IDEA.
+Создайте перечислительный тип (enum) Animals, содержащий конструктор,
+который должен принимать целочисленное значение (возраст животного),
+и содержать перегруженный метод toString(), который должен возвращать название экземпляра и возраст животного.
+ */
 public class Main {
     public static void main(String[] args) {
         for (Animals animal : Animals.values()) {
