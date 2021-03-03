65013554308d3993376046302506b08a83b51e64
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2021-03-03 21:25:42 +0200

    MASTER-7: added lesson9

---
+++ src/main/java/com/java_essential/ready_tasks/lesson9/ex_003_class_object_default_equals/Car.java
@@ -0,0 +1,10 @@
+package com.java_essential.ready_tasks.lesson9.ex_003_class_object_default_equals;
+
+public class Car {
+    private int id;
+
+    public Car(int id) {
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/com/java_essential/ready_tasks/lesson9/ex_005_class_object_hash_code/Main.java
@@ -0,0 +1,15 @@
+package com.java_essential.ready_tasks.lesson9.ex_005_class_object_hash_code;
+
+public class Main {
+    public static void main(String[] args) {
+        // создаем объекты, 2 из них содержат одинаковые параметры
+        Car car1 = new Car(12);
+        Car car2 = new Car(12);
+        Car car3 = new Car(15);
+
+        // вызываем метод hashCode, каков будет результат?
+        System.out.println(car1.hashCode());
+        System.out.println(car2.hashCode());
+        System.out.println(car3.hashCode());
+    }
+}
