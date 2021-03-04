60ea26ac325a06994a281ed70659555f35861d6d
Yuliia <yuliyapnivchuk@gmail.com>
2021-03-04 08:44:02 +0200

    HOMEWORK-7: message

--- src/main/java/com/java_essential/Class_7/Task_2/Main.java
+++ src/main/java/com/java_essential/Class_7/Task_2/Main.java
@@ -1,7 +1,6 @@
 package com.java_essential.Class_7.Task_2;
 
 public class Main {
-
     public static void main(String[] args) {
         Vehicle v1 = Vehicle.CAR;
         Vehicle v2 = Vehicle.PLANE;
