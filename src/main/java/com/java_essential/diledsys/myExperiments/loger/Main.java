9d5dcb40ad81442abd031cbc43e855735c64e915
diledsys <diledsys@gmil.com>
2021-03-05 19:19:08 +0300

    SimpleLoger

---
+++ src/main/java/com/java_essential/diledsys/myExperiments/loger/Main.java
@@ -0,0 +1,27 @@
+package com.java_essential.diledsys.myExperiments.loger;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int c=0,a=0,b=0;
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            try {
+                a = reader.nextInt();
+                b = reader.nextInt();
+                c = a / b;
+            } catch (ArithmeticException e) {
+                SimpleLog.log("деление на ноль \n" + e);
+                System.out.println("деление на ноль" + a + "/" + b);
+            } catch (InputMismatchException ie) {
+                SimpleLog.log(String.valueOf(ie));
+                break;
+            }
+            System.out.println(c);
+
+        }
+    }
+}
---
+++ src/main/java/com/java_essential/diledsys/myExperiments/loger/SimpleLog.java
@@ -0,0 +1,39 @@
+package com.java_essential.diledsys.myExperiments.loger;
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.Calendar;
+import java.util.Date;
+
+public class SimpleLog {
+    private static FileOutputStream fis;
+    public static void log(String str)  {
+        if (fis == null)getPath();
+        try {
+            Calendar calendar = Calendar.getInstance();
+            String d ="\n"+calendar.getTime()+"\n";
+            fis.write(d.getBytes());
+            fis.write(str.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void getPath()  {
+        File currentClass = null;
+        try {
+            currentClass = new File(URLDecoder.decode(SimpleLog.class
+                    .getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath(), "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        new File(currentClass.getPath()+"\\log\\").mkdir();
+        try {
+            fis = new FileOutputStream(new File(currentClass.getPath()+"\\log\\"+"log.txt"),true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
