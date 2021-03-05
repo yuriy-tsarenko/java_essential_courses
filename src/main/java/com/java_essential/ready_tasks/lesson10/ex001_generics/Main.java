fd6931ed59b5e21c4a4c5c28a44e6390fd3545f8
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2021-03-05 21:13:27 +0200

    DEVELOPMENT-9: added lesson 10 examples

---
+++ src/main/java/com/java_essential/ready_tasks/lesson10/ex001_generics/Main.java
@@ -0,0 +1,32 @@
+package com.java_essential.ready_tasks.lesson10.ex001_generics;
+
+/**
+ * Универсальные шаблоны.
+ * На 8-й строке создаем класс с именем MyClass, параметризированный указателем места заполнения типом - T
+ */
+class MyClass<T, R> {
+    public T field;
+    public R fieldTwo;
+
+    public void method() {
+        System.out.println(field.getClass());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр класса MyClass и в качестве параметра типа (тип MyClass) передаем тип String.
+        MyClass<String, String> instance1 = new MyClass<>();
+        instance1.field = "ABC";
+        instance1.method();
+
+        //// В качестве параметра типа передавать примитивные типы (int, long, float, double, и так далее) нельзя.
+        //MyClass<int> instance2 = new MyClass<int>();
+        //instance2.method();
+
+        // Создаем экземпляр класса MyClass и в качестве параметра типа (тип MyClass) передаем тип Integer.
+        MyClass<Integer, String> instance2 = new MyClass<Integer, String>();
+        instance2.field = 1234;
+        instance2.method();
+    }
+}
